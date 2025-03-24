dc3ba165fb3540cb1861222c8d67fbd792f14a8b
fragment78 <adrien.bourdin@hotmail.fr>
2025-03-24 05:18:57 +0100

    Update and rename index.js to api/proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  const { url, query } = req;
+  const baseUrl = "https://api.dofusdb.fr" + req.url.split("?")[0];
+
+  const searchParams = new URLSearchParams(query);
+
+  if (!searchParams.has("lang")) {
+    searchParams.set("lang", "fr");
+  }
+
+  if (!searchParams.has("limit")) {
+    searchParams.set("limit", "1000");
+  }
+
+  const fullUrl = `${baseUrl}?${searchParams.toString()}`;
+
+  try {
+    const response = await fetch(fullUrl);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Erreur proxy", detail: err.message });
+  }
+}
